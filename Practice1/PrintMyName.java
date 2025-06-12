d06b6c7cc501adbd6dbaa6091cecd560a27192f6
Daniyar <55020983+Baksharov-Daniyar@users.noreply.github.com>
2025-06-12 16:58:42 +0600

    Update PrintMyName.java

--- Practice1/PrintMyName.java
+++ Practice1/PrintMyName.java
@@ -1,1 +1,10 @@
+package Lab1;
 
+public class PrintMyName {
+    public static void main(String[] args) {
+        System.out.println("+-------+");
+        System.out.println("|Daniyar|");
+        System.out.println("+-------+");
+        
+    }
+}
